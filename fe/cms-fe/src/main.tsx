a904b88b40c1d49a43781f77fd3112fbf1f60fca
Pradip <dellpidarp57@gmail.com>
2024-09-30 07:12:52 +0545

    form handling done for login form through register method

--- fe/cms-fe/src/main.tsx
+++ fe/cms-fe/src/main.tsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./assets/global.css"
+import LoginPage from "./pages/auth/login/login.pages";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(<StrictMode>
+<LoginPage/>
+</StrictMode>);
